86e245a3c04b8c45c918e59d0eec6ec63d0ae661
Deleon <delps.santos1987@gmail.com>
2024-01-01 20:24:48 -0300

    gerador de tabela funcionando

--- Tabelas em Python.py
+++ Tabelas em Python.py
@@ -1,17 +1,70 @@
-#estou desenvolvondo um novo projeto utilizando tabela em pysiplegui
-
 import PySimpleGUI as sg
+import pandas as pd
+lista = [
+    
+]
+titulos = ["N°", "Item", "EAN", "Descrição do Produto", "QTD", "Preço"]
 
-lista=[["1","101","78900000101","acucar uniao de 1kg","1","3.90"]]
-titulos=["N°","Item","EAN","Descrição","QTD","Preço"]
+tabela = [
+    [sg.Table(values=lista, headings=titulos, max_col_width=10, auto_size_columns=True,
+              display_row_numbers=True, justification="right", num_rows=30, key="-TABELA-", row_height=12)],
+]
 
-layout= [
-    [sg.Table(values = lista,headings=titulos,max_col_width=10,auto_size_columns=True, display_row_numbers=True,justification = "right", num_rows=30,
-              key="-TABELA-",row_height=20)],
-    [sg.Button("SAIR")],]
+entradas = [
+    [sg.Text("     ENTRADA DE ITENS", font=("ANY", 18))],
+    [sg.Text("")],
+    [sg.Text("")],
+    [sg.Text("")],
 
-window = sg.Window("Tabela com valores", layout,size=(600,400))
-evento, valor = window.read()
+    [sg.Text("Numero", size=(15, 1),font=("Any",12)), sg.Input(size=(15, 1),font=("Any",12), key="-N-")],
+    [sg.Text("Item", size=(15, 1),font=("Any",12)), sg.Input(size=(15, 1),font=("Any",12), key="-ITEM-")],
+    [sg.Text("Ean", size=(15, 1),font=("Any",12)), sg.Input(size=(15, 1),font=("Any",12), key="-EAN-")],
+    [sg.Text("Descrição", size=(15, 1),font=("Any",12)), sg.Input(size=(15, 1),font=("Any",12), key="-DESC-")],
+    [sg.Text("Quantidade", size=(15, 1),font=("Any",12)), sg.Input(size=(15, 1),font=("Any",12), key="-QTD-")],
+    [sg.Text("Preço", size=(15, 1),font=("Any",12)), sg.Input(size=(15, 1),font=("Any",12), key="-PRECO-")],
+    [sg.Text("")],
+    [sg.Text("")],
+    [sg.Text("")],
+    [sg.Button("REGISTRAR",size=(12)),sg.Button("PLANILHA",size=(12)), sg.Button("SAIR",size=(7))]
+]
+
+layout = [
+    [sg.Col(entradas), sg.Col(tabela)],
+]
+
+window = sg.Window("Tabela com valores", layout, size=(810, 400), resizable=True)
+
 while True:
-    if evento in (sg.WIN_CLOSED,"SAIR"):
+    event, values = window.read()
+
+    if event in (sg.WIN_CLOSED, "SAIR"):
         break
+    elif event == "REGISTRAR":
+        # Obter valores dos campos de entrada
+        novo_registro = [
+            values["-N-"],
+            values["-ITEM-"],
+            values["-EAN-"],
+            values["-DESC-"],
+            values["-QTD-"],
+            values["-PRECO-"]
+        ]
+
+        # Adicionar os valores à lista
+        lista.append(novo_registro)
+
+        # Atualizar a tabela com os novos valores
+        window["-TABELA-"].update(values=lista)
+
+
+    elif event == "GERAR PLANILHA":
+        df = pd.DataFrame(lista[1:], columns=titulos)
+        file_path = sg.popup_get_file("Salvar Planilha com Python", save_as=True, default_extension=".xlsx")
+        
+        if file_path:
+            with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
+                df.to_excel(writer, index=False)
+                pd.save()
+        sg.popup("Planilha salva")
+
+window.close()
